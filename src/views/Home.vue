36ef470ecce1e2bca7afe00a66a617420dfdc13b
hartmut-at-143715418366 <hartmut-at-143715418366>
2019-10-06 19:11:27 +0200

    vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Hgo List"/>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi quidem ad minus maxime maiores dolores expedita praesentium cupiditate ipsam. Consequatur ipsam accusantium praesentium voluptas quod asperiores et corporis molestias! Adipisci!</p>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '../components/HelloWorld';
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
